9b7d67a1c0d1f2dc26af663f49e908e53a4836bc
yuievv <99083027@mydavinci.nl>
2026-09-06 21:54:27 +0200

    Voeg feestlunch.py toe
    Berekening kosten feestlunch

---
+++ 1 - De Basis/01 - Omgaan met gegevens/feestlunch.py
@@ -0,0 +1,35 @@
+"""
+Feestlunch.py
+
+Bereken de kosten van een feestlunch met de volgende producten:
+- 17 croissantjes van elk 0,39 euro
+- 2 stokbroden van elk 2,78 euro
+- 3 kortingsbonnen van elk 0,50 euro
+"""
+
+# aantal van producten en prijzen
+aantal_croissants = 17
+prijs_croissant = 0.39          # euro
+
+aantal_stokbroden = 2
+prijs_stokbrood = 2.78          # euro
+
+aantal_kortingsbonnen = 3
+waarde_kortingsbon = 0.50       # euro
+
+# berekening
+totaal_croissants = aantal_croissants * prijs_croissant
+totaal_stokbroden = aantal_stokbroden * prijs_stokbrood
+totaal_korting = aantal_kortingsbonnen * waarde_kortingsbon
+
+subtotaal = totaal_croissants + totaal_stokbroden
+te_betalen = subtotaal - totaal_korting
+
+# resultaat tonen
+print(". ݁₊ ⊹ . ݁ ⟡ ݁ Feestlunch ݁ ⟡ ݁ . ⊹ ₊ ݁.")
+print(f"{aantal_croissants} croissantjes    : € {totaal_croissants:.2f}")
+print(f"{aantal_stokbroden} stokbroden       : € {totaal_stokbroden:.2f}")
+print(f"Subtotaal          : € {subtotaal:.2f}")
+print(f"Korting ({aantal_kortingsbonnen} bonnen) : - € {totaal_korting:.2f}")
+print("—————————————————————————————————")
+print(f"Te betalen         : € {te_betalen:.2f}")
