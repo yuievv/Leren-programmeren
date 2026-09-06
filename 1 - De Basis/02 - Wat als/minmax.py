f8f82be26896e43f31e335cf23b8a67d713f7155
yuievv <99083027@mydavinci.nl>
2026-09-07 00:14:07 +0200

    min en max

--- 1 - De Basis/02 - Wat als/minmax.py
+++ 1 - De Basis/02 - Wat als/minmax.py
@@ -1,11 +1,25 @@
+"""
+Minmax.py
+
+Vraagt twee gehele getallen op (a en b) en bepaalt met if/elif/else
+welk getal het grootst en welk getal het kleinst is.
+"""
+
 a = int(input("Geef getal a: "))
 b = int(input("Geef getal b: "))
 
 if a > b:
     Max = a
+    Min = b
     print("a is het grootste getal:", Max)
 elif a < b:
     Min = a
+    Max = b
     print("a is het kleinste getal:", Min)
 else:
+    Max = a
+    Min = b
     print("a en b zijn even groot")
+
+print("Het minimum is:", Min)
+print("Het maximum is:", Max)
