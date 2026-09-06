3231ef863bf40881f56ebcbd184f49d4f04937a8
yuievv <99083027@mydavinci.nl>
2026-09-07 01:31:36 +0200

    Verbeter pizzacalculator met invoervalidatie en bon-filter
    Toon specifieke foutmelding bij niet-heel-getallen

--- 1 - De Basis/03 - Foute pizza met uitzonderingen/pizzacalculator.py
+++ 1 - De Basis/03 - Foute pizza met uitzonderingen/pizzacalculator.py
@@ -1,7 +1,6 @@
-
 small_prijs = 6.99
 medium_prijs = 8.25
-large_prijs = 11.10
+large_prijs = 10.89
 
 def vraag_aantal_pizza(afmeting):
     while True:
@@ -12,18 +11,24 @@ def vraag_aantal_pizza(afmeting):
             else:
                 return aantal
         except ValueError:
-            print("Ongeldige invoer. Probeer opnieuw.")
+            print("Dit is geen heel nummer!")
 
 def bereken_prijs(aantal, prijs):
     return aantal * prijs
 
 def print_bon(small_aantal, medium_aantal, large_aantal):
     print(". ݁₊ ⊹ . ݁ ⟡ ݁ Kassabon ݁ ⟡ ݁ . ⊹ ₊ ݁.")
-    print(f"Pizza's small: {small_aantal} x {small_prijs:.2f} =  {bereken_prijs(small_aantal, small_prijs):.2f}")
-    print(f"Pizza's medium: {medium_aantal} x {medium_prijs:.2f} = {bereken_prijs(medium_aantal, medium_prijs):.2f}")
-    print(f"Pizza's large: {large_aantal} x {large_prijs:.2f} = {bereken_prijs(large_aantal, large_prijs):.2f}")
+    
+    if small_aantal > 0:
+        print(f"Pizza's small: {small_aantal} x {small_prijs:.2f} =  {bereken_prijs(small_aantal, small_prijs):.2f}")
+    if medium_aantal > 0:
+        print(f"Pizza's medium: {medium_aantal} x {medium_prijs:.2f} = {bereken_prijs(medium_aantal, medium_prijs):.2f}")
+    if large_aantal > 0:
+        print(f"Pizza's large: {large_aantal} x {large_prijs:.2f} = {bereken_prijs(large_aantal, large_prijs):.2f}")
+    
     print("—————————————————————————————————")
-    print(f"Totaalprijs:               {bereken_prijs(small_aantal, small_prijs) + bereken_prijs(medium_aantal, medium_prijs) + bereken_prijs(large_aantal, large_prijs):.2f}")
+    totaal = bereken_prijs(small_aantal, small_prijs) + bereken_prijs(medium_aantal, medium_prijs) + bereken_prijs(large_aantal, large_prijs)
+    print(f"Totaalprijs:               {totaal:.2f}")
 
 def main():
     small_aantal = vraag_aantal_pizza("small")
