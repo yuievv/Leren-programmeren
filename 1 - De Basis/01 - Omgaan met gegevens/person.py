7689770c0257bf8e3e7d6287461f669a2cae24eb
yuievv <99083027@mydavinci.nl>
2026-09-06 23:38:10 +0200

    Voeg person.py toe
    Vraagt naam, adres, postcode en woonplaats en toont adreskaartje

---
+++ 1 - De Basis/01 - Omgaan met gegevens/person.py
@@ -0,0 +1,16 @@
+naam = input("Wat is je naam? ")
+adres = input("Wat is je adres (straat + huisnummer)? ")
+postcode = input("Wat is je postcode? ")
+woonplaats = input("In welke woonplaats woon je? ")
+
+breedte = 52
+
+print("")
+print(" ݁₊ ⊹ . ݁ ⟡ ݁ Adreskaartje ݁ ⟡ ݁ . ⊹ ₊ ݁.")
+print(" " + "-" * breedte)
+print(f"|  Naam      : {naam}")
+print(f"|  Adres     : {adres}")
+print(f"|  Postcode  : {postcode}")
+print(f"|  Woonplaats: {woonplaats}")
+print(" " + "-" * breedte)
+print("")
